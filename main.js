b3e4cf90748ba8cb002808a80828b4a0cb3959a2
Brian Wright <briankw23@gmail.com>
2018-03-10 13:57:18 -0600

    WIP

---
+++ main.js
@@ -0,0 +1,5 @@
+var example = {"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", "new":"nytt", "year":"år"};
+var inputv = document.getElementById("input").value;
+console.log(inputv);
+
+
